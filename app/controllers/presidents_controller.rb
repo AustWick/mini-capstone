2ebfa16f7df4855da4f8e04b72522fcfd8a5def9
“AustWick” <austin.wicketts@gmail.com>
2017-08-29 20:34:04 -0500

    added new repo

--- app/controllers/presidents_controller.rb
+++ app/controllers/presidents_controller.rb
@@ -1,2 +1,11 @@
 class PresidentsController < ApplicationController
+  def one_president_action
+    @president = President.last
+    render 'one_president_page.html.erb'
+  end
+
+  def all_presidents_action
+    @president = President.all
+    render 'all_presidents_page.html.erb'
+  end
 end
---
+++ db/migrate/20170830011410_create_presidents.rb
@@ -0,0 +1,14 @@
+class CreatePresidents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :presidents do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :price
+      t.string :chef
+      t.string :description
+      t.string :termination
+
+      t.timestamps
+    end
+  end
+end
